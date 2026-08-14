9d25b9a46ff422d4f7bb518f5d40284f05d18ef2
paulinhoraposo <paulinhoraposo@gmail.com>
2026-08-13 23:57:05 -0300

    chore: setup Dockerfile e Next standalone para deploy no Easypanel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.0.0.191', '192.168.2.14', 'localhost:3000'],
+  output: 'standalone',
 };
 
 export default nextConfig;
